91ea653d776a0b362b190a22730bb349b34636c3
Erich Keil <erich@zenlexlife.com>
2021-10-09 20:04:38 -0500

    Data Fetch and D3 Import

--- barchart/app.js
+++ barchart/app.js
@@ -1,2 +1,22 @@
+import * as d3 from "https://cdn.skypack.dev/d3@7";
 const chart = document.querySelector(".container.barchart");
-chart.textContent = "hello world";
+const url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+//get data
+const data = fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+//create svg
+
+const width = 600; //change this width/height to be responsive
+const height = 400;
+const svg = d3.create("svg").attr([0, 0, width, height]);
+
+//map data to d3
+
+//map data to rectangles
+
+//style
+
+//label
