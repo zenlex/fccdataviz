2736676415fbfc8c37596067d22e38cc21766805
zenlex <erichrkeil@gmail.com>
2021-11-15 21:46:26 -0600

    add axes

--- heatmap/app.js
+++ heatmap/app.js
@@ -24,4 +24,33 @@ function render(data) {
   heading.append('h3')
     .attr('id', 'description')
     .text(`${data.monthlyVariance[0].year} - ${data.monthlyVariance[data.monthlyVariance.length - 1].year} Base temperature = ${data.baseTemperature}\u00B0 C`);
+
+  /*--------------------------------------
+  CREATE AXES & SCALES
+ ---------------------------------------*/
+  const height = 400;
+  const width = 1200;
+  const padding = 60;
+
+  const years = data.monthlyVariance.map(({ year, month }) => new Date(year, month - 1));
+
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(years))
+    .range([0, width]);
+
+  const yScale = d3.scaleTime()
+    .domain([new Date(2021, 0), new Date(2021, 11)])
+    .range([0, height - 2 * padding]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%B'));
+
+  const svg = section.append('svg').attr('x', 0).attr('y', 0).attr('width', width)
+    .attr('height', height);
+
+  svg.append('g').attr('transform', `translate(${padding},${height - padding})`).attr('id', 'x-axis').call(xAxis);
+
+  svg.append('g').attr('transform', `translate(${padding}, ${padding})`).attr('id', 'y-axis').call(yAxis);
+
+  svg.append('y');
 } // end render function
