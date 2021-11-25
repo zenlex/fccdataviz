2733c1ce93ac26954fe48ab6624f53a6dffa32e7
zenlex <erichrkeil@gmail.com>
2021-11-24 21:38:45 -0600

    basic map geoinformation rendered

--- src/js/choropleth.js
+++ src/js/choropleth.js
@@ -4,29 +4,52 @@
 // headings
 import * as d3 from 'd3';
 import d3Tip from 'd3-tip';
+import * as topojson from 'topojson';
 import '../style/choropleth.css';
 
-const usURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const edDataURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
 
-const countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+const countiesGeoURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
 
-const usData = d3.json(usURL).catch((err) => console.log(err));
-const countyData = d3.json(countyURL).catch((err) => console.log(err));
+const edData = d3.json(edDataURL).catch((err) => console.log(err));
+const geoData = d3.json(countiesGeoURL).catch((err) => console.log(err));
 
 Promise.all([
-  usData,
-  countyData,
+  edData,
+  geoData,
 ]).then((values) => render(values[0], values[1]));
 
 const tip = d3Tip();
 const svgh = 500;
 const svgw = 1000;
 
-console.log(tip, svgh, svgw);
+function render(edD, geoD) {
+  const path = d3.geoPath();
+  const container = d3.select('#choropleth-container');
+  container.append('svg');
 
-d3.select('#choropleth-container').append('text').text('hello world');
+  const svg = d3.select('svg');
+  svg.attr('height', svgh)
+    .attr('width', svgw);
 
-function render(usD, coD) {
-  console.log('usData = ', usD);
-  console.log('countyData =', coD);
+  svg
+    .append('g')
+    .attr('class', 'counties')
+    .selectAll('path')
+    .data(topojson.feature(geoD, geoD.objects.counties).features)
+    .enter()
+    .append('path')
+    .attr('class', 'county')
+    .attr('d', path)
+    .style('stroke', '#fff');
+
+  svg.append('path')
+    .datum(
+      topojson.mesh(geoD, geoD.objects.states, (a, b) => a !== b),
+    )
+    .attr('class', 'states')
+    .attr('d', path);
+
+  console.log('usData = ', edD);
+  console.log('countyData =', geoD.objects.counties);
 } // end render function
